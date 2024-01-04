18c53a945e085c683695480a7c2a095b6daf28f9
aldbrnstdy <aldebaranstudy97@gmail.com>
2024-01-05 00:01:07 +0700

    add functionality insert to table postgresql

--- dags/api_to_staging.py
+++ dags/api_to_staging.py
@@ -7,8 +7,18 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from ingest.ingest_data import Ingest
 import pandas as pd 
 import json
+import psycopg2
+from sqlalchemy import create_engine
+
 
 # df_cities = pd.read_csv('./dags/file_utils/kabupaten_kota_jawa_barat.csv')
+
+host="localhost"
+database="jabar_openweather"
+user="ahyar"
+password="shanpark203"
+port=int(float('5432')) # in integer
+db = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
 def ingest_all_cities(ti):
     df_cities = pd.read_csv('./dags/file_utils/kabupaten_kota_jawa_barat.csv')
     responses_city = []
@@ -31,6 +41,7 @@ def ingest_all_cities(ti):
         time_sunrise = data["sys"].get("sunrise",None)
         time_sunset = data["sys"].get("sunset",None)
         response_dict ={
+            "city_id":city["city_id"],
             "city_name":city["city_name"],
             "weather_main" : weather_main,
             "weather_description" :weather_description,
@@ -51,21 +62,38 @@ def ingest_all_cities(ti):
     # df_dt.to_csv('./outputtry.csv')
     ti.xcom_push(key='responses_city',value=responses_city)
 
-def insert_to_temp(ti):
+def insert_to_stg_temperature(ti):
     # Retrieve the XCom values
     xcom_values = ti.xcom_pull(task_ids='ingest_all_cities_data',key='responses_city')
     data_f = pd.DataFrame.from_dict(xcom_values)
-    data_f.to_csv('./outputtry.csv')
-    # sql_query = f"INSERT INTO weather (city_name,description,temperature,windspeed) VALUES ('{key1_value}', '{key2_value}','{key3_value}','{key4_value}');"
+    data_temp = data_f[["city_name","weather_pressure","weather_humidity","weather_sea_level","weather_grnd_level"]]
+    data_temp.to_csv('./data_temp.csv')
+    data_temp.to_sql(
+        name="stg_temperature",con=db,schema="staging",if_exists='append', index=False
+    )
     
-    # hook = PostgresHook(postgres_conn_id='postgres_localhost')
-    # hook.run(sql_query)
-#create default args
+def insert_to_stg_wind(ti):
+    xcom_values = ti.xcom_pull(task_ids='ingest_all_cities_data',key='responses_city')
+    data_f = pd.DataFrame.from_dict(xcom_values)
+    data_wind = data_f[["city_id","weather_wind_speed","weather_wind_gust","time_dim"]]
+    data_wind.to_sql(
+        name="stg_wind",con=db,schema="staging",if_exists='append', index=False
+    )
+
+def insert_to_stg_weather(ti):
+    xcom_values = ti.xcom_pull(task_ids='ingest_all_cities_data',key='responses_city')
+    data_f = pd.DataFrame.from_dict(xcom_values)
+    data_weather = data_f[["city_id","weather_main","weather_description","weather_temp","weather_temp_feels_like"]]
+    data_weather.to_sql(
+        name="stg_weather",con=db,schema="staging",if_exists='append', index=False
+    )
+    data_weather.to_csv('weather.csv')
+
 
 default_args = {
     'owner': 'ahyar',
     'retries': 5,
-    'retry_delay': timedelta(minutes=2)
+    'retry_delay': timedelta(minutes=8)
 }
 
 
@@ -83,8 +111,18 @@ with DAG(
         dag=dag
     )
     task2=PythonOperator(
-        task_id='insert_to_temp',
-        python_callable=insert_to_temp,
+        task_id='insert_to_stg_temperature',
+        python_callable=insert_to_stg_temperature,
+        dag=dag
+    )
+    task3=PythonOperator(
+        task_id='insert_to_stg_wind',
+        python_callable=insert_to_stg_wind,
+        dag=dag
+    )
+    task4=PythonOperator(
+        task_id='insert_to_stg_weather',
+        python_callable=insert_to_stg_weather,
         dag=dag
     )
     # task3= PythonOperator(
@@ -95,7 +133,7 @@ with DAG(
 # task1.set_downstream(task2)
 # task1.set_downstream(task3)
 
-task1 >> task2
+task1 >> [task2,task3,task4]
 # task2 >> task3
 
 # task1 >> [task2,task3]
